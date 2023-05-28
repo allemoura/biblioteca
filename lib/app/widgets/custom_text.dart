a7d62ed2793a5ff6c835fb69e8889941d04b2272
Alessandra Barros <33128122+allemoura@users.noreply.github.com>
2023-05-28 14:45:28 -0300

    fit: confirm raffle dialog

---
+++ lib/app/widgets/custom_text.dart
@@ -0,0 +1,29 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String value;
+  final Color? textColor;
+  final TextStyle? textStyle;
+  final FontWeight? fontWeight;
+  final double? fontSize;
+  const CustomText(
+      {Key? key,
+      required this.value,
+      this.textColor = Colors.black,
+      this.textStyle,
+      this.fontWeight = FontWeight.normal,
+      this.fontSize})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: value,
+        style: TextStyle(
+            color: textColor, fontSize: fontSize, fontWeight: fontWeight),
+      ),
+    );
+  }
+}
