70acb422a4d9e75d529372fdf5af7525e8520370
Alessandra Barros <33128122+allemoura@users.noreply.github.com>
2023-06-03 12:03:17 -0300

    configure firebase to android and ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:biblioteca/app/pages/main_page.dart';
 import 'package:biblioteca/theme/custom_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
